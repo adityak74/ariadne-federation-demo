899208e2e2dad10b23bbdcc52a064a5ace4e7b9e
Aditya Karnam <akarnam37@gmail.com>
2022-10-31 23:00:47 -0500

    feat: add flask example, custom error

--- microservices/service_users.py
+++ microservices/service_users.py
@@ -1,6 +1,8 @@
 import uvicorn
 
+from ariadne import format_error
 from ariadne import QueryType
+from ariadne.graphql import GraphQLError
 from ariadne.asgi import GraphQL
 from ariadne.contrib.federation import FederatedObjectType, make_federated_schema
 
@@ -30,8 +32,20 @@ def resolve_user_reference(_, _info, representation):
     return get_user_by_email(representation.get("email"))
 
 
+def my_format_error(error: GraphQLError, debug: bool = False) -> dict:
+    if debug:
+        # If debug is enabled, reuse Ariadne's formatting logic (not required)
+        return format_error(error, debug)
+
+    # Create formatted error data
+    formatted = error.formatted
+    # Replace original error message with custom one
+    formatted["message"] = "INTERNAL SERVER ERROR"
+    return formatted
+
+
 schema = make_federated_schema(type_defs, [query, user])
-application = GraphQL(schema)
+application = GraphQL(schema, error_formatter=my_format_error)
 
 users = [
     {"id": 1, "name": "Ada Lovelace", "email": "ada@example.com"},
